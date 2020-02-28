f1b79e49647883a731333db1b56027407b55f637
Thomas Lem <lemt@oregonstate.edu>
2020-02-27 16:58:44 -0800

    added function for area of circle

--- task.py
+++ task.py
@@ -1,2 +1,12 @@
+import math
+
+
 def firstrun():
     return "success"
+
+
+def circle(radius):
+    area = radius ** 2 * math.pi
+    return round(area, 2)
+
+
--- tests.py
+++ tests.py
@@ -12,6 +12,10 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def test_circle(self):
+        self.area = task.circle(5)
+        self.assertEqual(78.54, self.area)
+
 
 if __name__ == '__main__':
     unittest.main()
