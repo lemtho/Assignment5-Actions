2a592ced24db9451b2260c25746cc485a8727290
Thomas Lem <lemt@oregonstate.edu>
2020-02-27 17:39:47 -0800

    added daysbetween function

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import math
+from datetime import date
 
 
 def firstrun():
@@ -12,3 +13,10 @@ def circle(radius):
 
 def firstlast(e_list):
     return [e_list[0], e_list[-1]]
+
+
+def daysbetween(year1, month1, day1, year2, month2, day2):
+    first_date = date(year1, month1, day1)
+    second_date = date(year2, month2, day2)
+    delta = second_date - first_date
+    return delta.days
--- tests.py
+++ tests.py
@@ -17,10 +17,14 @@ class TestCase(unittest.TestCase):
         self.assertEqual(78.54, self.area)
 
     def test_firstlast(self):
-        self.e_list = ["Kobe", 2, 4, 8, "Gigi"]
+        self.e_list = ["Kobe", 2, 24, 8, "Gigi"]
         self.firstlast = task.firstlast(self.e_list)
         self.test_list = ["Kobe", "Gigi"]
         self.assertListEqual(self.firstlast, self.test_list)
+
+    def test_date(self):
+        self.days_between = task.daysbetween(2019, 1, 23, 2020, 1, 23)
+        self.assertEqual(365, self.days_between)
 
 
 if __name__ == '__main__':
